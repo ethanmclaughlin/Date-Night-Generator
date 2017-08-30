6c234464c1d8ce45b47d31d5b91afefe415a8e44
CIS Student <cisstudent@CISs-Mac-mini-2.local>
2017-08-30 15:12:15 -0500

    Initial Commit

---
+++ Date Night Generator/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Date Night Generator
+//
+//  Created by CIS Student on 8/30/17.
+//  Copyright © 2017 Freed-hardeman University. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
